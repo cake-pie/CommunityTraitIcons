648fd52cb9b5c9e3d7f1685443a86fd0f3250cf9
cake-pie <cake-pie@users.noreply.github.com>
2017-06-22 05:44:47 +0800

    version

---
+++ Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle ("CommunityTraitIcons")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("CommunityTraitIcons")]
+[assembly: AssemblyCopyright ("Copyright ©  2017")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+[assembly: AssemblyVersion ("0.1.0.0")]
+[assembly: KSPAssembly("CommunityTraitIcons", 0, 1)]
